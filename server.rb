a96940be279d01211f914c2a015c468c3a1b1ccd
Rimas Silkaitis <rimas@chartio.com>
2015-07-31 00:31:20 -0700

    be sure the connection doesnt go bad

--- server.rb
+++ server.rb
@@ -24,24 +24,29 @@ get '/' do
   @orders       = []
 
   if !badconnection
-    @openorders = conn.exec("select count(*) as order_count from orders where o_orderstatus = 'O' and o_orderdate = '1998-08-02'")
-    @openorders = @openorders[0]['order_count'].to_i
+    begin
+      @openorders = conn.exec("select count(*) as order_count from orders where o_orderstatus = 'O' and o_orderdate = '1998-08-02'")
+      @openorders = @openorders[0]['order_count'].to_i
 
-    @failedorders = conn.exec("select count(*) as order_count from orders where o_orderstatus = 'F' and o_orderdate = '1995-06-15'")
-    @failedorders = @failedorders[0]['order_count'].to_i
+      @failedorders = conn.exec("select count(*) as order_count from orders where o_orderstatus = 'F' and o_orderdate = '1995-06-15'")
+      @failedorders = @failedorders[0]['order_count'].to_i
 
-    conn.exec( "select o_orderdate as dt, sum(o_totalprice) as total from orders group by 1 order by 1 desc limit 10" ) do |result|
-      result.each do |r|
-        @orders << {
-          'timeframe' => {
-            'start' => r['dt'],
-            'end'   => r['dt']
-          },
-          'value' => [{
-            'result' => (r['total'].to_f / 1000000.0).to_i
-          }]
-        }
+      conn.exec( "select o_orderdate as dt, sum(o_totalprice) as total from orders group by 1 order by 1 desc limit 10" ) do |result|
+        result.each do |r|
+          @orders << {
+            'timeframe' => {
+              'start' => r['dt'],
+              'end'   => r['dt']
+            },
+            'value' => [{
+              'result' => (r['total'].to_f / 1000000.0).to_i
+            }]
+          }
+        end
       end
+    rescue => e
+      badconnection = true
+      connection_err = e.message
     end
   end
   erb :index, :layout => :application, :locals => { :badconnection => badconnection, :connection_err => connection_err, :page => 'dashboard'}
@@ -50,16 +55,21 @@ end
 get '/customers' do
   @customers = []
   if !badconnection
-    @customers = conn.exec(<<-SQL
-      select c_name as name
-           , c_address as address
-           , c_mktsegment as market_segment
-           , c_acctbal as account_balance
-        from customer
-       where c_acctbal > 0
-       limit 30
-    SQL
-    )
+    begin
+      @customers = conn.exec(<<-SQL
+        select c_name as name
+             , c_address as address
+             , c_mktsegment as market_segment
+             , c_acctbal as account_balance
+          from customer
+         where c_acctbal > 0
+         limit 30
+      SQL
+      )
+    rescue => e
+      badconnection = true
+      connection_err = e.message
+    end
   end
 
   erb :customers, :layout => :application, :locals => {:badconnection => badconnection, :connection_err => connection_err, :page => 'customers'}
